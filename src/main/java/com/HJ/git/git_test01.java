6116b88389d51069cd722db667b27e5ef9853e06
steven <1140800025@qq.com>
2023-03-19 22:19:39 +0800

    third commit

--- src/main/java/com/HJ/git/git_test01.java
+++ src/main/java/com/HJ/git/git_test01.java
@@ -8,5 +8,6 @@ public class git_test01 {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
